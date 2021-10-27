84ce7b6dda433ae6c9315c6ffb54138a17eda353
anderson.sdelgado@gmail.com <anderson.sdelgado@gmail.com>
2021-10-27 10:03:42 -0300

    Ajuste de Biblioteca conexão Serial - Chamado 16902

--- src/util/ConnSerial.java
+++ src/util/ConnSerial.java
@@ -5,23 +5,21 @@
  */
 package util;
 
+import com.fazecast.jSerialComm.SerialPort;
 import control.BigBagCTR;
-import gnu.io.CommPortIdentifier;
-import gnu.io.SerialPort;
-import gnu.io.SerialPortEvent;
-import gnu.io.SerialPortEventListener;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import model.domain.Balanca;
 
 /**
  *
  * @author anderson
  */
-public class ConnSerial implements Runnable, SerialPortEventListener {
+public class ConnSerial implements Runnable {
 
     public int nodeBytes;
-    private CommPortIdentifier cp;
-    private SerialPort porta;
+    private SerialPort serialPort;
     private InputStream entrada;
     private Thread threadLeitura;
     private Balanca balanca;
@@ -34,39 +32,19 @@ public class ConnSerial implements Runnable, SerialPortEventListener {
 
     public void inicialConexao() {
         try {
-            cp = CommPortIdentifier.getPortIdentifier("COM1");
-            System.out.println("Obteve a  porta = COM1");
-            if (cp == null) {
-                System.out.println("A COM1 nao existe!");
-                System.exit(1);
-            }
-            System.out.println("Porta abrir");
-            porta = (SerialPort) cp.open("SComm", 1000);
-            System.out.println("Porta aberta com sucesso!");
 
-            porta.setSerialPortParams(9600, porta.DATABITS_8, porta.STOPBITS_1, porta.PARITY_NONE);
+            serialPort = SerialPort.getCommPort("COM1");
+            serialPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
+            serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+            serialPort.openPort();
 
-            try {
-                entrada = porta.getInputStream();
-                System.out.println("FLUXO OK!");
-            } catch (Exception e) {
-                System.out.println("Erro.STATUS: " + e);
-                System.exit(1);
-            }
-            try {
-                porta.addEventListener(this);
-                System.out.println("SUCESSO. Porta aguardando...");
-            } catch (Exception e) {
-                System.out.println("Erro ao criar listener: ");
-                System.out.println("STATUS: " + e);
-                System.exit(1);
-            }
-            porta.notifyOnDataAvailable(true);
-            try {
+            if (serialPort.isOpen()) {
+                entrada = serialPort.getInputStream();
                 threadLeitura = new Thread(this);
                 threadLeitura.start();
-            } catch (Exception e) {
-                System.out.println("Erro ao iniciar leitura: " + e);
+                System.out.println(serialPort.getDescriptivePortName() + " SUCESSO. Porta aguardando...");
+            } else {
+                System.out.println(serialPort.getDescriptivePortName() + " FALHA DE ABERTURA ");
             }
 
         } catch (Exception e) {
@@ -76,50 +54,22 @@ public class ConnSerial implements Runnable, SerialPortEventListener {
     }
 
     public void run() {
+        BufferedReader in = new BufferedReader(new InputStreamReader(entrada));
         try {
-            Thread.sleep(5000);
+            while (true) {
+                String dadosRec = in.readLine();
+                System.out.println("Retorno = " + dadosRec);
+                bigBagCTR.salvarEnsaqueBigBag(dadosRec, balanca);
+            }
+//            in.close();
         } catch (Exception e) {
             System.out.println("Erro. Status = " + e);
         }
     }
 
-    public void serialEvent(SerialPortEvent ev) {
-        switch (ev.getEventType()) {
-            case SerialPortEvent.BI:
-            case SerialPortEvent.OE:
-            case SerialPortEvent.FE:
-            case SerialPortEvent.PE:
-            case SerialPortEvent.CD:
-            case SerialPortEvent.CTS:
-            case SerialPortEvent.DSR:
-            case SerialPortEvent.RI:
-            case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
-                break;
-            case SerialPortEvent.DATA_AVAILABLE:
-                byte[] bufferLeitura = new byte[20];
-                try {
-                    while (entrada.available() > 0) {
-                        nodeBytes = entrada.read(bufferLeitura);
-                    }
-                    String dadosRec = new String(bufferLeitura);
-                    if (bufferLeitura.length == 0) {
-                        System.out.println("Nada lido!");
-                    } else if (bufferLeitura.length == 1) {
-                        System.out.println("Apenas um byte foi lido!");
-                    } else {
-                        System.out.println("Dados recebido: " + dadosRec);
-                        bigBagCTR.salvarEnsaqueBigBag(dadosRec, balanca);
-                    }
-                } catch (Exception e) {
-                    System.out.println("Erro durante a leitura: " + e);
-                }
-                break;
-        }
-    }
-
     public void FecharCom() {
         try {
-            porta.close();
+            serialPort.closePort();
             System.out.println("CONEXAO FECHADA>>FIM..");
         } catch (Exception e) {
             System.out.println("ERRO AO FECHAR. STATUS: " + e);
